92c65be655f303d5512f3fdd221eea1a42e0d9ab
Julianeves01 <julianvs2007@gmail.com>
2025-04-08 20:22:27 -0300

    jesus nao funciona

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { CreateDrawerNavigator } from ".@react-navigation/drawer";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import { HomeScreen } from "./pages/home";
-import DetailsScreen from "./screens/DetailsScreen";
+import DetailsScreen from "./pages/details";
 
-const Drawer = CreateDrawerNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
