10dd90406728aac4de2460dd4da2279b26a5ae88
Julianeves01 <julianvs2007@gmail.com>
2025-04-14 16:02:36 -0300

    update Drawer2.js

---
+++ pages/Drawer2.js
@@ -0,0 +1,41 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+export default function DrawerScreen() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}> Ola amores! </Text>
+            <Text style={styles.subtitle}> Você está navegando no Drawer Screen 2! </Text>
+            <Text style={styles.description}>Sejam bem vindos! 🎀</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#d298c4",
+        padding: 20,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    title: {
+        marginBottom: 10,
+        fontSize: 30,
+        fontWeight: "bold",
+        color: "#fff",
+    },
+    subtitle: {
+        fontSize: 20,
+        color: "#fff",
+        textAlign: "center",
+        marginBottom: 15,
+        paddingHorizontal: 15,
+    },
+    description: {
+        fontSize: 30,
+        color: "#fff",
+        textAlign: "center",
+        paddingHorizontal: 20,
+    },
+});
