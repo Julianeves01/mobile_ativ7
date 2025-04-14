74b596193324df532502857bca584f55541a076e
Julianeves01 <julianvs2007@gmail.com>
2025-04-14 15:55:24 -0300

    update App.js

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-
-import { HomeScreen } from "./pages/home";
-import DetailsScreen from "./pages/details";
-
-const Drawer = createDrawerNavigator();
+import Drawer from "./navigation/DrawerNavigator";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-      <Drawer.Screen name="Home" component={HomeScreen} />
-      <Drawer.Screen name="Details" component={DetailsScreen} />
-      </Drawer.Navigator>
+      <Drawer />
     </NavigationContainer>
   );
 }
